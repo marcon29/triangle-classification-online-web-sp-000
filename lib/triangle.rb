c64fbcc16b2ad3965130c62f1e22e4ebf56c5804
Mark Stabler <mark@markstabler.com>
2020-03-20 05:45:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  attr_accessor :side_one, :side_two, :side_three
+
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+    
+  end
+
+  def kind
+    sides = [side_one, side_two, side_three]
+    case 
+      when sides.all? { |s| s == sides[0] }
+        :equilateral
+      when
+        :isosceles
+      when
+        :scalene
+    end
+  end
+
+
+  #class KindError
+  #end
+
+
+  
 end
