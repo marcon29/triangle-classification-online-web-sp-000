9ae3d1b38b524e1a657a6f9433f4d181e41f447d
Mark Stabler <mark@markstabler.com>
2020-03-20 06:28:34 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  attr_accessor :side_one, :side_two, :side_three
+
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+  end
+
+  class TriangleError < StandardError
+    "That's not a triangle"
+  end
+
+  def kind
+    sides = [side_one, side_two, side_three]
+
+    if sides.sort[0] + sides.sort[1] <= sides.sort[2] || !sides.all?{ |s| s > 0 }
+      raise TriangleError
+    else
+      case
+        when sides.sort[2] == sides.sort[0]
+          :equilateral
+        when sides.sort[2] == sides.sort[1] && sides.sort[2] != sides.sort[0]
+          :isosceles
+        when sides.sort[2] != sides.sort[1]
+          :scalene
+      end
+    end
+  end
 end
